5a733cd045113dca4ba389c9f61ca3aecc926601
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-12-26 16:34:30 +0200

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+from hashlib import sha256
+from datetime import datetime
+
+time = datetime.now()
+
+
+class Block():
+    pass
+
+
+class Blockchain():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
