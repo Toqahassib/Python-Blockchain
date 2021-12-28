25a1dc037f1dd3ce118b020b5010cbfcb691b4d8
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-12-28 13:33:43 +0200

    added the registration function

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
+from sql import *
+from forms import *
 
 # initialize the app
 
@@ -18,12 +20,57 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # initialize mysql
 mysql = MySQL(app)
 
+
+def login(username):
+    users = Table("users", "name", "email", "username", "password")
+    user = users.getone("username", username)
+
+    session['logged_in'] = True
+    session['username'] = username
+    session['name'] = user.get('name')
+    session['email'] = user.get('email')
+
+# registration page
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm(request.form)
+    users = Table("users", "name", "email", "username", "password")
+
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        name = form.name.data
+        # check if new user
+        if isnewuser(username):
+            password = sha256_crypt.encrypt(form.password.data)
+            users.insert(name, email, username, password)
+            login(username)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('User already exists', 'danger')
+            return redirect(url_for('register'))
+
+    return render_template('register.html', form=form)
+
+# create a dashboard page
+
+
+@app.route("/dashboard")
+def dashboard():
+    # a created table
+
+    return render_template('dashboard.html', session=session)
+
 # create homepage
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    # a created table
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
