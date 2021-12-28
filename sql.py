5ded8ec99dc08335a7d14c144ce60294e7b14345
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-12-28 20:48:06 +0200

    sending money feature added

--- sql.py
+++ sql.py
@@ -1,20 +1,35 @@
-# this file is to simplify using mysql with python
-from MySQLdb.cursors import Cursor
+from os import sync
 from app import mysql, session
 from blockchain import Block, Blockchain
 
+# custom exceptions for transaction errors
+
+
+class InvalidTransactionException(Exception):
+    pass
+
+
+class InsufficientFundsException(Exception):
+    pass
+
 
 class Table():
-    def __init__(self, tbl_name, *args):
-        self.table = tbl_name
-        self.columns = "(%s)" % ",".join(args)
 
-        # check if table exists
-        if tbl_exists(tbl_name):
-            cursor = mysql.connection.cursor()
-            cursor.execute("CREATE TABLE {}{}".format(
-                self.table, self.columns))
-            cursor.close()
+    def __init__(self, table_name, *args):
+        self.table = table_name
+        self.columns = "(%s)" % ",".join(args)
+        self.columnsList = args
+
+        # if table does not already exist, create it.
+        if isnewtable(table_name):
+            create_data = ""
+            for column in self.columnsList:
+                create_data += "%s varchar(100)," % column
+
+            cur = mysql.connection.cursor()  # create the table
+            cur.execute("CREATE TABLE %s(%s)" %
+                        (self.table, create_data[:len(create_data)-1]))
+            cur.close()
 
     # get all the values from the table
     def getall(self):
@@ -23,8 +38,8 @@ class Table():
         data = cur.fetchall()
         return data
 
-    # get from the table using column's data
-    # e.g getone("hash","000438jjk70fv...")
+    # get one value from the table based on a column's data
+    # EXAMPLE using blockchain: ...getone("hash","00003f73gh93...")
     def getone(self, search, value):
         data = {}
         cur = mysql.connection.cursor()
@@ -35,7 +50,7 @@ class Table():
         cur.close()
         return data
 
-    # delete from the table using a column's data
+    # delete a value from the table based on column's data
     def deleteone(self, search, value):
         cur = mysql.connection.cursor()
         cur.execute("DELETE from %s where %s = \"%s\"" %
@@ -43,7 +58,7 @@ class Table():
         mysql.connection.commit()
         cur.close()
 
-    # delete all from the table.
+    # delete all values from the table.
     def deleteall(self):
         self.drop()  # remove table and recreate
         self.__init__(self.table, *self.columnsList)
@@ -54,7 +69,7 @@ class Table():
         cur.execute("DROP TABLE %s" % self.table)
         cur.close()
 
-    # insert into the table
+    # insert values into the table
     def insert(self, *args):
         data = ""
         for arg in args:  # convert data into string mysql format
@@ -75,15 +90,103 @@ def sql_raw(execution):
     mysql.connection.commit()
     cur.close()
 
+# check if table already exists
 
-def tbl_exists(tableName):
-    cursor = mysql.connection.cursor()
-    try:
-        result = cursor.execute("SELECT * FROM {}".format(tableName))
-        cursor.close()
-    # table doesn't exist
+
+def isnewtable(tableName):
+    cur = mysql.connection.cursor()
+
+    try:  # attempt to get data from table
+        result = cur.execute("SELECT * from %s" % tableName)
+        cur.close()
     except:
         return True
-    # table exists
     else:
         return False
+
+# check if user already exists
+
+
+def isnewuser(username):
+    # access the users table and get all values from column "username"
+    users = Table("users", "name", "email", "username", "password")
+    data = users.getall()
+    usernames = [user.get('username') for user in data]
+
+    return False if username in usernames else True
+
+# send money from one user to another
+
+
+def send_money(sender, reciever, amount):
+    # verify that the amount is an integer or floating value
+    try:
+        amount = float(amount)
+    except ValueError:
+        raise InvalidTransactionException("Invalid Transaction.")
+
+    # verify that the user has enough money to send (exception if it is the BANK)
+    if amount > get_balance(sender) and sender != "BANK":
+        raise InsufficientFundsException("Insufficient Funds.")
+
+    # verify that the user is not sending money to themselves or amount is less than or 0
+    elif sender == reciever or amount <= 0.00:
+        raise InvalidTransactionException("Invalid Transaction.")
+
+    # verify that the recipient exists
+    elif isnewuser(reciever):
+        raise InvalidTransactionException("User Does Not Exist.")
+
+    # update the blockchain and sync to mysql
+    blockchain = get_blockchain()
+    number = len(blockchain.chain) + 1
+    transaction = "%s-->%s-->%s" % (sender, reciever, amount)
+    blockchain.mining(Block(number, transaction=transaction))
+    sync_blockchain(blockchain)
+
+# get the balance of a user
+
+
+def get_balance(username):
+    balance = 0.00
+    blockchain = get_blockchain()
+
+    # loop through the blockchain and update balance
+    for block in blockchain.chain:
+        data = block.transaction.split("-->")
+        if username == data[0]:
+            balance -= float(data[2])
+        elif username == data[1]:
+            balance += float(data[2])
+    return balance
+
+# get the blockchain from mysql and convert to Blockchain object
+
+
+def get_blockchain():
+    blockchain = Blockchain()
+    blockchain_sql = Table("blockchain", "number", "hash",
+                           "previous", "transaction", "nonce", "timestamp")
+    for b in blockchain_sql.getall():
+        blockchain.add(Block(int(b.get('number')), b.get(
+            'previous'), b.get('transaction'), int(b.get('nonce')), b.get('timestamp')))
+
+    return blockchain
+
+# update blockchain in mysql table
+
+
+def sync_blockchain(blockchain):
+    blockchain_sql = Table("blockchain", "number", "hash",
+                           "previous", "transaction", "nonce", "timestamp")
+    blockchain_sql.deleteall()
+
+    for block in blockchain.chain:
+        blockchain_sql.insert(str(block.number), block.hash(
+        ), block.prev_hash, block.transaction, block.nonce, block.timestamp)
+
+
+def test():
+    blockchain_sql = Table("blockchain", "number", "hash",
+                           "previous", "transaction", "nonce", "timestamp")
+    blockchain_sql.deleteall()
