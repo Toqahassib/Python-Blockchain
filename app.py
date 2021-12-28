be4983432bf8d6f01bf19cbc2c758e52087ba064
Toqahassib <70861082+Toqahassib@users.noreply.github.com>
2021-12-28 13:38:32 +0200

    moved the registration class here

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 from sql import *
-from forms import *
+from wtforms import Form, StringField, DecimalField, IntegerField, TextAreaField, PasswordField, validators
+
+
+class RegisterForm(Form):
+    name = StringField('Full Name', [validators.length(min=1, max=50)])
+    username = StringField('Username', [validators.length(min=4, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [validators.DataRequired(
+    ), validators.EqualTo('confirm', message='Password dont match')])
+    confirm = PasswordField('Confirm Password')
+
 
 # initialize the app
 
